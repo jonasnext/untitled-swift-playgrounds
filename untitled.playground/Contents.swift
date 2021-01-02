ed8a2dfd8231c4f71fe2e9e6b71575943e2a087c
jonas <jonasnext@users.noreply.github.com>
2021-01-02 21:03:59 +0100

    untitled swift playgrounds

---
+++ untitled.playground/Contents.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
